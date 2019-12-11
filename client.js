3ab8cee364075a659e6e3ae5fd2f3601ca9bdcd3
Oleg <olegsolovev506@gmail.com>
2019-12-11 13:30:59 +0400

    finished mapping

--- client.js
+++ client.js
@@ -1,86 +1,107 @@
-
 ///////////////////////////////////////////////////////////////////////////////////////
-var https = require('http'),
-  io_server = require('socket.io');
-var fs = require('fs');
+var https = require("http"),
+  io_server = require("socket.io");
+var fs = require("fs");
 ///////////////////////////////////////////////////////////////////////////////////////
 // FOR HTTPS
 var options = {
-  key: fs.readFileSync('./real-keys/key.pem'),
-  cert: fs.readFileSync('./real-keys/cert.pem')
+  key: fs.readFileSync("./real-keys/key.pem"),
+  cert: fs.readFileSync("./real-keys/cert.pem")
 };
 ///////////////////////////////////////////////////////////////////////////////////////
 // CLIENT SIDE
-var io_client = require('socket.io-client');
-var socket_client = io_client.connect('http://localhost:9559', { reconnect: true });
-socket_client.on('connect', function () {
-  console.log('Proxy connected to server');
+var io_client = require("socket.io-client");
+var socket_client = io_client.connect("http://localhost:9559", {
+  reconnect: true
+});
+socket_client.on("connect", function() {
+  console.log("Proxy connected to server");
 });
 ///////////////////////////////////////////////////////////////////////////////////////
 // SERVER SIDE
-var server = https.createServer(function (req, res) {
-  res.writeHead(404, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from Proxy!</h1>');
+var server = https.createServer(function(req, res) {
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.end("<h1>Hello from Proxy!</h1>");
 });
 
 server.listen(8088);
 io_server = io_server.listen(server, {
   log: true,
-  origins: '*:*'
+  origins: "*:*"
 });
 
-io_server.set('transports', [
-  'xhr-polling',
-  'jsonp-polling'
-]);
+io_server.set("transports", ["xhr-polling", "jsonp-polling"]);
 
-io_server.sockets.on('connection', function (socket_server) {
-  console.log('Client connected to proxy');
+io_server.sockets.on("connection", function(socket_server) {
+  console.log("Client connected to proxy");
   ////////////////////////////////////////////////
   // Here we receive requests from actual server to our proxy and forward them to clients
   socket_client.on("response", data => {
-    socket_server.emit("response", data)
-  })
+    console.log("response from server to proxy");
+    socket_server.emit("response", data);
+  });
   socket_client.on("presence", isChannelPresent => {
-    socket_server.emit('presence', isChannelPresent);
-  })
+    console.log("presence from server to proxy");
+    socket_server.emit("presence", isChannelPresent);
+  });
   socket_client.on("connect", isConnected => {
-    socket_server.emit('connect', true);
-  })
+    console.log("connect from server to proxy");
+    socket_server.emit("connect", true);
+  });
   ////////////////////////////////////////////////
   //Here we recevie requests from actual clients to our proxy and forward them to server
   socket_server.on("request", data => {
-    console.log('Request from client to server');
+    console.log("Request from client to server");
     socket_client.emit("request", data);
-  })
+  });
   socket_server.on("new-channel", data => {
-    console.log('New channel from client to server');
+    console.log("New channel from client to server");
     socket_client.emit("new-channel", data);
-  })
+    ////////////////////////////////////
+    const namespace = io_server.of("/" + data.channel);
+    const nsp_socket = io_client.connect(
+      `http://localhost:9559/${data.channel}`,
+      { reconnect: true }
+    );
+    namespace.on("connection", function(socket_inside_namespace) {
+      console.log("connect to channel: " + data.channel);
+      nsp_socket.emit("connection");
+
+      nsp_socket.on("connect", payload => {
+        console.log(`connect to ${data.channel} from server to proxy`);
+        socket_inside_namespace.emit("connect", true);
+      });
+
+      nsp_socket.on("message", payload => {
+        console.log(`message to ${data.channel} from server to proxy`);
+        socket_inside_namespace.broadcast.emit("message", payload);
+      });
+
+      nsp_socket.on("user-left", payload => {
+        console.log(`user-left to ${data.channel} from server to proxy`);
+        socket_inside_namespace.broadcast.emit("user-left", payload);
+      });
+
+      socket_inside_namespace.on("message", payload => {
+        console.log(`message to ${data.channel} from client to proxy`);
+        nsp_socket.emit("message", payload);
+      });
+
+      socket_inside_namespace.on("disconnect", payload => {
+        console.log(`disconnect to ${data.channel} from client to proxy`);
+        nsp_socket.emit("disconnect", payload);
+      });
+    });
+    ////////////////////////////////////
+  });
   socket_server.on("disconnect", data => {
-    console.log('Disconnect from client to server');
+    console.log("Disconnect from client to server");
     socket_client.emit("disconnect", data);
-  })
+  });
   socket_server.on("presence", data => {
-    console.log('Presence from client to server');
+    console.log("Presence from client to server");
     socket_client.emit("presence", data);
-  })
-  ////////////////////////////////////////////////
-  function onNewNamespace(channel, sender) {
-    io_server.of('/' + channel).on('connection', function (socket) {
-      console.log("connect to channel: " + channel);
-
-      socket.on('message', function (data) {
-        console.log("mesage from channel: " + channel);
-        socket_client.emit("presence", data.data);
-      });
-
-      socket.on('disconnect', function () {
-        console.log("mesage from channel: " + channel);
-        socket_client.emit("disconnect");
-      });
-    })
-  }
+  });
   ////////////////////////////////////////////////
 });
 ///////////////////////////////////////////////////////////////////////////////////////
